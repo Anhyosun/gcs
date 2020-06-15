6d92c8d4d80af279924a522f0352b8f90969d720
Anhyosun <57958010+Anhyosun@users.noreply.github.com>
2020-06-16 01:56:06 +0900

    구구단 업데이트. 단 출력 및 개행 표시

--- study/src/study/gugudan/gugudan.java
+++ study/src/study/gugudan/gugudan.java
@@ -3,9 +3,11 @@ package study.gugudan;
 public class gugudan {
 	public static void main(String[] args) {
 		for(int i=1;i<10;i++) {
+			System.out.println("*****" + i + "´Ü*******");
 			for(int j=1;j<10;j++) {
 				System.out.println(i + " * " + j + " = " + i*j);
 			}
+			System.out.println();
 		}
 	}
 }
